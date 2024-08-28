654c86bcb24ce475e5b870614c26133c76ab6bfb
Matthew Antone <matt.antone@gmail.com>
2024-08-28 10:44:53 -0700

    updated home page query

--- lib/queries/pages.ts
+++ lib/queries/pages.ts
@@ -3,7 +3,7 @@ import { groq } from "next-sanity";
 export const PAGES_QUERY = groq`*[_type == "page" && defined(slug)]`;
 export const PAGE_QUERY = groq`*[_type == "page" && slug.current == $slug][0]`;
 
-export const HOME_PAGE_QUERY = groq`*[_type == "home" && slug.current == "home"][0]{
+export const HOME_PAGE_QUERY = groq`*[_type == "home"][0]{
   ...,
   gallery[]{
     ...,
