40e24089cc1c64533c18f4250514d9ee6d0e5f68
Matthew Antone <matt.antone@gmail.com>
2024-09-19 15:32:07 -0700

    more debugging

--- src/app/api/update/updateAlgoliaPost.ts
+++ src/app/api/update/updateAlgoliaPost.ts
@@ -4,20 +4,20 @@ const appID = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string;
 // API key with `addObject` and `editSettings` ACL
 const apiKey = process.env.ALGOLIA_SEARCH_ADMIN_KEY as string;
 
-
-export const updateAlgoliaPost = async (index:string, post: any) => {
+export const updateAlgoliaPost = async (index: string, post: any) => {
+  console.log("creating client", { index, post });
+  const client = algoliasearch(appID, apiKey);
   try {
-    console.log("updating post", {index, post})
-    const client = algoliasearch(appID, apiKey);
+    console.log("updating post", { index, post });
     // Add record to an index
     const algoliaResponse = await client.replaceAllObjects({
       indexName: index,
       objects: [post],
     });
-    console.log({ algoliaResponse });
+    console.log(algoliaResponse);
     console.log(`🎉 Sucessfully added records to Algolia search (${index}).`);
-    return algoliaResponse
+    return algoliaResponse;
   } catch (error) {
-    console.log(error);     
+    console.log(error,{appID, apiKey});
   }
-}
+};
